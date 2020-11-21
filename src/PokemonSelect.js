dd246d3b35ad410d6fe5c0e5e5f310c7b25c65a0
unknown <jreidmke@gmail.com>
2020-11-21 11:53:15 -0600

    commenting notes

--- src/PokemonSelect.js
+++ src/PokemonSelect.js
@@ -3,7 +3,8 @@ import pokemonList from "./pokemonList";
 import { choice } from "./helpers";
 
 /* Select element to choose from common pokemon. */
-function PokemonSelect({ add, pokemon = pokemonList })//notice that the props have been set and pokemon has a defaultProp of pokemonList imported above {
+function PokemonSelect({ add, pokemon = pokemonList })//notice that the props have been set and pokemon has a defaultProp of pokemonList imported above
+{
   const [pokeIdx, setPokeIdx] = useState(0);//first the pokeIdx is set to 0. let's see where this comes in handy.
   const handleChange = evt => {//pretty basic handleChange function right here.
     setPokeIdx(evt.target.value);//sets the pokeIdx state to whatever the value returned from the select form is.
@@ -11,14 +12,14 @@ function PokemonSelect({ add, pokemon = pokemonList })//notice that the props ha
 
   return (
     <div>
-      <select onChange={handleChange}>
-        {pokemon.map((p, idx) => (
-          <option key={idx} value={idx}>
+      <select onChange={handleChange}> {/**handleChange passed in */}
+        {pokemon.map((p, idx) => ( //the pokemon array imported above is passed in to create values in select form.
+          <option key={idx} value={idx}> {/**key and value are set automatically created idx */}
             {p}
           </option>
         ))}
       </select>
-      <button onClick={() => add(pokemon[pokeIdx])}>Catch one!</button>
+      <button onClick={() => add(pokemon[pokeIdx])}>Catch one!</button> {/**look it! i was right. the add prop get's an argument passed in of selected pokemon. ok. this making sense */}
       <button onClick={() => add(choice(pokemon))}>I'm feeling lucky</button>
     </div>
   );
